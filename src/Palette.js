e5057ef2b71481e1b35d3fecd7d39621edffe3fb
LeeTrent <lee.trent.1@gmail.com>
2019-09-05 08:25:59 -0500

    1. Creates project; 2. Adds Palette component;

---
+++ src/Palette.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export default class Palette extends Component {
+    render() {
+        return (
+            <div className="Palette">
+                <h1>PALETTE</h1>
+                { /* NavBar goes here */ }
+                <div className="Palette-colors">
+                    { /* bunch of color boxes */ }
+                </div>
+                { /* footer goes here */ }
+            </div>
+        )
+    }
+}
