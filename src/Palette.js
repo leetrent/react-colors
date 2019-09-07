706566916933948b7b61a943b81ab1bc65e44372
LeeTrent <lee.trent.1@gmail.com>
2019-09-07 11:29:12 -0500

    Adds copy-overlay animation styling

--- src/Palette.js
+++ src/Palette.js
@@ -9,7 +9,6 @@ export default class Palette extends Component {
         ));
         return (
             <div className="Palette">
-                <h1>PALETTE</h1>
                 { /* NavBar goes here */ }
                 <div className="Palette-colors">
                     { /* bunch of color boxes */ colorBoxes}
