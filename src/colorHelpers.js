81424e12010eb0d16a35363b89bcd3f4bcd5fb45
LeeTrent <lee.trent.1@gmail.com>
2019-09-09 11:43:34 -0500

    Adds colorHelpers.js to generate different shades of the same color

---
+++ src/colorHelpers.js
@@ -0,0 +1,40 @@
+import chroma from 'chroma-js';
+
+const levels = [50, 100, 200, 300, 400, 500, 600, 700 , 800, 900];
+
+function generatePalette(starterPalette) {
+    let newPalette = {
+        paletteName: starterPalette.paletteName,
+        id: starterPalette.id,
+        emoji: starterPalette.emoji,
+        colors: {}
+    };
+    for ( let level of levels) {
+        newPalette.colors[level] = [];
+    }
+    for ( let color of starterPalette.colors ) {
+        let scale = getScale(color.color, 10).reverse();
+        for ( let ii in scale ) {
+            newPalette.colors[levels[ii]].push({
+                name: `${color.name} ${levels[ii]}`,
+                id: color.name.toLowerCase().replace(/ /g, "-"),
+                hex: scale[ii],
+                rgb: chroma(scale[ii]).css(),
+                rgba: chroma(scale[ii]).css().replace("rgb", "rgba").replace(")", ",1.0)")
+
+            });
+        }
+    }
+    return newPalette;
+}
+
+function getScale(hexColor, numberOfColors) {
+    return chroma.scale(getRange(hexColor)).mode('lab').colors(numberOfColors);
+}
+
+function getRange(hexColor) {
+    const endColor = "#fff";
+    return [ chroma(hexColor).darken(1.4).hex(), hexColor, endColor ];
+}
+
+export { generatePalette };
