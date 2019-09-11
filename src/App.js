087eb3cb286845de88a9745a3c7e25c3077aea9f
LeeTrent <lee.trent.1@gmail.com>
2019-09-11 10:10:44 -0500

    Adds Material UI React library and uses the Select and MenuItem components in Navbar component

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Palette palette={ generatePalette(seedColors[4]) } />
+        <Palette palette={ generatePalette(seedColors[0]) } />
       </div>
     )
   } 
--- src/Palette.js
+++ src/Palette.js
@@ -6,23 +6,31 @@ import "./Palette.css";
 export default class Palette extends Component {
     constructor(props) {
         super(props);
-        this.state = { level: 500 };
+        this.state = { level: 500, format: 'hex' };
         this.changeLevel = this.changeLevel.bind(this);
+        this.changeFormat = this.changeFormat.bind(this);
     }
     changeLevel(newLevel) {
         console.log("newLevel", newLevel);
         this.setState( { level: newLevel } );
     }
+    changeFormat(val) {
+        this.setState( { format: val } );
+    }
     render() {
         const { colors } = this.props.palette;
-        const { level } = this.state;
+        const { level, format } = this.state;
         const colorBoxes = colors[level].map(color => (
-            <ColorBox background={color.hex} name={color.name} />
+            <ColorBox background={ color[format] } name={color.name} />
         ));
         return (
             <div className="Palette">
                 { /* NavBar goes here */ }
-                <Navbar level={level} changeLevel={this.changeLevel}/>
+                <Navbar 
+                    level={level} 
+                    changeLevel={this.changeLevel} 
+                    handleChange={this.changeFormat}
+                />
                 <div className="Palette-colors">
                     { /* bunch of color boxes */ colorBoxes}
                 </div>
