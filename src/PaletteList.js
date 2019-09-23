4c3c8a795a54b79cc4262dd07dad9d65107b9f00
LeeTrent <lee.trent.1@gmail.com>
2019-09-23 07:29:22 -0500

    Links MiniPalette onClick event to individual Palette component

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -33,6 +33,9 @@ const styles = {
 };
 
 class PaletteList extends Component {
+    goToPalette(id) {
+        this.props.history.push(`/palette/${id}`);
+    }
     render() {
         const { palettes, classes } = this.props;
         return (
@@ -43,7 +46,8 @@ class PaletteList extends Component {
                     </nav>
                     <div className={classes.palettes}>
                         { palettes.map(palette => (
-                            <MiniPalette key={palette.paletteName} { ...palette } />
+                            <MiniPalette key={palette.paletteName} { ...palette } 
+                                handleClick={ () => this.goToPalette(palette.id) } />
                         ))}   
                     </div>
                 </div>
